10a06b2fede4d796b31b1c79c771796d6a772b9d
bhavishya plawat <tobhavishya2004@gmail.com>
2024-08-08 12:40:33 +0530

    update:completed faulty calc

--- exercise/FaultyCalcl.js
+++ exercise/FaultyCalcl.js
@@ -1,14 +1,47 @@
+/*Question-create a faulty calculator using javascript
+
+this faulty calculator does
+1. take 2 number input from user
+2. it performs wrong operation as follow
+   + --> -
+   - --> /
+   * --> +
+   / --> **(exonential)
+
+   errors will be made only 10% of times ,90% correct calculator
+
+*/
 let FirstNum = prompt("enter 1st number");
 let SecNum = prompt("enter 2nd number");
 let operator = prompt("enter any operation + , - , / , * .");
 console.log("you have entered " + FirstNum + " " + operator + " " + SecNum);
 
-if ((operator = "+")) {
-  console.log(parseInt(FirstNum) - parseInt(SecNum));
-} else if ((operator = "*")) {
-  console.log(parseInt(FirstNum) + parseInt(SecNum));
-} else if ((operator = "-")) {
-  console.log(parseInt(FirstNum) / parseInt(SecNum));
-} else if ((operator = "/")) {
-  console.log(parseInt(FirstNum) ** parseInt(SecNum));
+const FaultyCalc = (FirstNum, SecNum) => {
+  if ((operator = "+")) {
+    return parseInt(FirstNum) - parseInt(SecNum);
+  } else if ((operator = "*")) {
+    return parseInt(FirstNum) + parseInt(SecNum);
+  } else if ((operator = "-")) {
+    return parseInt(FirstNum) / parseInt(SecNum);
+  } else if ((operator = "/")) {
+    return parseInt(FirstNum) ** parseInt(SecNum);
+  }
+};
+
+const correctCalc = (FirstNum, SecNum) => {
+  if ((operator = "+")) {
+    return parseInt(FirstNum) + parseInt(SecNum);
+  } else if ((operator = "*")) {
+    return parseInt(FirstNum) * parseInt(SecNum);
+  } else if ((operator = "-")) {
+    return parseInt(FirstNum) - parseInt(SecNum);
+  } else if ((operator = "/")) {
+    return parseInt(FirstNum) / parseInt(SecNum);
+  }
+};
+
+if (Math.random() < 0.1) {
+  FaultyCalc(FirstNum, SecNum);
+} else {
+  correctCalc(FirstNum, SecNum);
 }
