eab71ec0d9f3a6f837e3d8e8b2b44cfa2fe8990b
bhavishya plawat <tobhavishya2004@gmail.com>
2024-08-13 22:37:36 +0530

    feet:Factorial logic

---
+++ exercise/Factorial.js
@@ -0,0 +1,11 @@
+/* 
+write a program to calculate factorial of number using for loops
+ */
+
+let n = prompt("Enter a number to get Factorial");
+let factorial = 1;
+for (let i = 1; i <= n; i++) {
+  factorial *= i;
+}
+console.log(factorial);
+//ik this was easy but i was assigned the task so here it is
